ad47fe7377184fb61752e6597dfed1691ee9f770
Anderson <85256569+SadaydZ@users.noreply.github.com>
2024-11-23 15:58:50 -0500

    non used

--- src/main/java/com/certus/challenge/controller/ControladorVistasPrinc.java
+++ src/main/java/com/certus/challenge/controller/ControladorVistasPrinc.java
@@ -2,8 +2,6 @@ package com.certus.challenge.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class ControladorVistasPrinc {
