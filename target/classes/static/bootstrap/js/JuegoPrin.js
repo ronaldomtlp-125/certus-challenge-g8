e805ed08872964a108fdaf38ee3294271fe5f1a4
Ronaldo Arrieta <ronaldomtlp125@gmail.com>
2024-12-05 19:46:43 -0500

    Tablero y dados juego
    Agregar nueva estructura para el tablero, integrar seccion de dados, sala de espera y reajuste a los botones de mercado, info y pausa.

--- target/classes/static/bootstrap/js/JuegoPrin.js
+++ target/classes/static/bootstrap/js/JuegoPrin.js
@@ -1,3 +1,20 @@
 document.querySelector('.jugador').addEventListener('click', function() {
     alert("¡Contenedor clickeado!");
 });
+
+function tirarDados() {
+    const dado1 = Math.floor(Math.random() * 6) + 1;
+    const dado2 = Math.floor(Math.random() * 6) + 1;
+    const dado3 = Math.floor(Math.random() * 6) + 1;
+    const dado4 = Math.floor(Math.random() * 6) + 1;
+    const dado5 = Math.floor(Math.random() * 6) + 1;
+    // Mostrar los números en los elementos correspondientes
+    document.getElementById('dado1').textContent = dado1;
+    document.getElementById('dado2').textContent = dado2;
+    document.getElementById('dado3').textContent = dado3;
+    document.getElementById('dado4').textContent = dado4;
+    document.getElementById('dado5').textContent = dado5;
+    // Calcular y mostrar el resultado
+    const resultado = dado1 + dado2 + dado3 + dado4 + dado5;
+    document.getElementById('resultado').textContent = resultado;
+}
